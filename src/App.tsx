33851eeb3f539beaea86053cf945f84b00e68431
John <mjohncorbet@gmail.com>
2024-02-25 13:34:49 +0800

    setup sass-loader, update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import HeaderDetails from './components/HeaderDetails';
 import ContactDetails from './components/ContactDetails';
+import './style/style.scss';
 
 export default function App() {
     return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,12 @@ export default defineConfig({
         watch: {
             usePolling: true
         }
-    }
+    },
+    css: {
+        preprocessorOptions: {
+          scss: {
+            implementation: sass,
+          },
+        },
+    },
 });
