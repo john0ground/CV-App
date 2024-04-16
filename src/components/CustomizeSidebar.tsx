1a72de87f4add8707537e7a3782fdf90aa2a2fe1
John <mjohncorbet@gmail.com>
2024-04-16 21:50:15 +0800

    style side-bar

--- src/components/CustomizeSidebar.tsx
+++ src/components/CustomizeSidebar.tsx
@@ -29,8 +29,19 @@ interface CustomizeSideBarProps {
 function LayoutsSection({layoutIndex, changeLayout}: LayoutsSectionProps) {
     return (
         <section className="layout-options">
-            <button data-active={layoutIndex === 0} onClick={ () => changeLayout(0) }>Layout 1</button>
-            <button data-active={layoutIndex === 1} onClick={ () => changeLayout(1) }>Layout 2</button>
+            <h2>Layout</h2>
+            <div className="options">
+                <button data-active={layoutIndex === 0} onClick={ () => changeLayout(0) }>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={layoutIndex === 1} onClick={ () => changeLayout(1) }>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+            </div>
         </section>
     );
 }
@@ -38,42 +49,45 @@ function LayoutsSection({layoutIndex, changeLayout}: LayoutsSectionProps) {
 function ThemesSection({themeIndex, changeTheme}: ThemesSectionProps) {
     return (
         <section className="theme-options">
-            <button data-active={themeIndex === 0} onClick={ () => changeTheme(0) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 1} onClick={ () => changeTheme(1) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 2} onClick={ () => changeTheme(2) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 3} onClick={ () => changeTheme(3) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 4} onClick={ () => changeTheme(4) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 5} onClick={ () => changeTheme(5) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 6} onClick={ () => changeTheme(6) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 7} onClick={ () => changeTheme(7) }>
-                <div></div>
-                <div></div>
-            </button>
-            <button data-active={themeIndex === 8} onClick={ () => changeTheme(8) }>
-                <div></div>
-                <div></div>
-            </button>
+            <h2>Themes</h2>
+            <div className="options">
+                <button data-active={themeIndex === 0} onClick={ () => changeTheme(0) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 1} onClick={ () => changeTheme(1) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 2} onClick={ () => changeTheme(2) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 3} onClick={ () => changeTheme(3) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 4} onClick={ () => changeTheme(4) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 5} onClick={ () => changeTheme(5) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 6} onClick={ () => changeTheme(6) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 7} onClick={ () => changeTheme(7) }>
+                    <div></div>
+                    <div></div>
+                </button>
+                <button data-active={themeIndex === 8} onClick={ () => changeTheme(8) }>
+                    <div></div>
+                    <div></div>
+                </button>
+            </div>
         </section>
     );
 }
@@ -81,10 +95,13 @@ function ThemesSection({themeIndex, changeTheme}: ThemesSectionProps) {
 function FontsSection({fontIndex, changeFont}: FontsSectionProps) {
     return (
         <section className="font-options">
-            <button data-active={fontIndex === 0} onClick={() => changeFont(0)}>Montserrat</button>
-            <button data-active={fontIndex === 1} onClick={() => changeFont(1)}>Open Sans</button>
-            <button data-active={fontIndex === 2} onClick={() => changeFont(2)}>Poppins</button>
-            <button data-active={fontIndex === 3} onClick={() => changeFont(3)}>Noto Sans</button>
+            <h2>Fonts</h2>
+            <div className="options">
+                <button data-active={fontIndex === 0} onClick={() => changeFont(0)}>Montserrat</button>
+                <button data-active={fontIndex === 1} onClick={() => changeFont(1)}>Open Sans</button>
+                <button data-active={fontIndex === 2} onClick={() => changeFont(2)}>Poppins</button>
+                <button data-active={fontIndex === 3} onClick={() => changeFont(3)}>Noto Sans</button>
+            </div>
         </section>
     );
 }
@@ -110,9 +127,11 @@ export default function CustomizeSideBar({
     return (
         <div ref={sideBarRef} className="side-bar" data-active={active} onClick={handleToggleBar}>
             <div className="customize-bar">
-                <LayoutsSection layoutIndex={layoutIndex} changeLayout={changeLayout} />
-                <ThemesSection themeIndex={themeIndex} changeTheme={changeTheme} />
-                <FontsSection fontIndex={fontIndex} changeFont={changeFont} />
+                <div className="container">
+                    <LayoutsSection layoutIndex={layoutIndex} changeLayout={changeLayout} />
+                    <ThemesSection themeIndex={themeIndex} changeTheme={changeTheme} />
+                    <FontsSection fontIndex={fontIndex} changeFont={changeFont} />
+                </div>
             </div>
         </div>
     );
